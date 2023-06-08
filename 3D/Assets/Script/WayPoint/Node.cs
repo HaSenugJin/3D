a719b607de088879d00979a99a3b757f6256af85
HaSenugJin <126733434+HaSenugJin@users.noreply.github.com>
2023-06-08 16:49:49 +0900

    060804

--- 3D/Assets/Script/WayPoint/Node.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent(typeof(SphereCollider))]
-public class Node : MonoBehaviour
-{
-    public Node Next;
-}
