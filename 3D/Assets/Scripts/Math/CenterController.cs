77d23543f1996e79c33c757afd2ee318951bb273
HaSenugJin <126733434+HaSenugJin@users.noreply.github.com>
2023-06-09 16:23:05 +0900

    06092

--- 3D/Assets/Scripts/Math/CenterController.cs
+++ 3D/Assets/Scripts/Math/CenterController.cs
@@ -4,45 +4,67 @@ using UnityEngine;
 
 public class CenterController : MonoBehaviour
 {
-/*
-    public List<GameObject> PointList;
-    
-    void Start()
+    /*
+public List<GameObject> PointList;
+
+void Start()
+{
+    for (int i = 0; i < 72; ++i)
     {
-        for (int i = 0; i < 72; ++i)
-        {
-            GameObject obj = new GameObject("point");
-    
-            obj.AddComponent<MyGizmo>();
-    
-            obj.transform.position = new Vector3(
-                Mathf.Sin((i * 5.0f) * Mathf.Deg2Rad),
-                Mathf.Cos((i * 5.0f) * Mathf.Deg2Rad),
-                0.0f) * 5.0f;
-    
-            PointList.Add(obj);
-        }
+        GameObject obj = new GameObject("point");
+
+        obj.AddComponent<MyGizmo>();
+
+        obj.transform.position = new Vector3(
+            Mathf.Sin((i * 5.0f) * Mathf.Deg2Rad),
+            Mathf.Cos((i * 5.0f) * Mathf.Deg2Rad),
+            0.0f) * 5.0f;
+
+        PointList.Add(obj);
+    }
+
+
+
 }
-*/
+    */
 
     [Range(-90.0f, 90.0f)]
     public float Angle;
+
+
+    [Range(-10.0f, 20.0f)]
+    public float X;
 
     private void Start()
     {
         gameObject.AddComponent<MyGizmo>();
 
+        X = 5.0f;
         Angle = 0.0f;
     }
 
+
     private void Update()
     {
-        float hor = Input.GetAxis("Horizontal");
+        Vector3 StartPoint = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 EndPoint = new Vector3(10.0f, 5.0f, 0.0f);
+
+        Debug.DrawLine(StartPoint, EndPoint);
+
+        float Width = EndPoint.x - StartPoint.x;
+        float Height = EndPoint.y - StartPoint.y;
+
+        //float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
-        Vector3 Movement = new Vector3(hor, Mathf.Sin
-            (Angle * Mathf.Deg2Rad) * 5.0f, ver) * 5.0f * Time.deltaTime;
+        float Y = 0.0f;
 
-        transform.Translate(Movement);
+        if (StartPoint.x < X && X < EndPoint.x)
+            Y = (Height / Width) * X;
+        
+
+        Vector3 Movement = new Vector3(X, Y, ver);
+
+        transform.position = Movement;
     }
 }
